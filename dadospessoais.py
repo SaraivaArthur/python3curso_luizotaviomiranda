4e1b6c5ab1e0604d7433899cb5e20d0e95648e29
Arthur Saraiva <142699700+SaraivaArthur@users.noreply.github.com>
2025-10-05 20:10:16 -0300

    Create dadospessoais.py

---
+++ dadospessoais.py
@@ -0,0 +1,13 @@
+nome = 'Arthur'
+sobrenome = 'Saraiva'
+idade = 16
+ano_de_nascimento = 2009
+maior_deidade = 'Sim'
+altura_metros = '1.68'
+
+print('Nome', nome)
+print('Sobrenome', sobrenome)
+print('Idade', idade)
+print('Ano de nascimento', ano_de_nascimento)
+print('É maior de idade?', maior_deidade)
+print('Altura em metros', altura_metros)
