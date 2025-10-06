0a0313a9ddf861a5fa69da2f961bf5f9af479fbb
Arthur Saraiva <142699700+SaraivaArthur@users.noreply.github.com>
2025-10-05 21:00:43 -0300

    Create calculoimc.py

---
+++ calculoimc.py
@@ -0,0 +1,8 @@
+nome = 'Arthur Saraiva'
+altura = 1.68
+peso = 62
+imc = ...
+'''IMC = peso / (altura x altura)'''
+
+imc = peso / altura ** altura
+print(nome, 'tem', altura, 'de altura e', 'pesa', peso, 'kg')
