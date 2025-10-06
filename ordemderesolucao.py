6818ac6788fb620374be8428779cd2a1802017b5
Arthur Saraiva <142699700+SaraivaArthur@users.noreply.github.com>
2025-10-05 21:01:08 -0300

    Create ordemderesolucao.py

---
+++ ordemderesolucao.py
@@ -0,0 +1,9 @@
+conta_1 = (1 + 1) ** (5 + 5)
+print(conta_1)
+#1.dentro de parenteses
+#2.Exponenciação
+#3.Multiplicação, divisão e módulo
+#4.Adição e subtração
+
+conta_2 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_2)
